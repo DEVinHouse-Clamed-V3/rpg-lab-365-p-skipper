6abf241cc1629800be4284172cd1997791cb5b79
p-skipper <kipperspedro@gmail.com>
2025-02-07 14:06:25 -0300

    feat: create class inimigo

---
+++ Inimigo.ts
@@ -0,0 +1,15 @@
+import Personagem from "./Personagem";
+
+class Inimigo extends Personagem {
+  atacar(alvo: Personagem): void {
+    if (Math.random() >= 0.2) {
+      alvo.receberDano(this.getForca() + (this.getArma() ? this.getArma()!.getDano() : 0));
+    }
+  }
+
+  comportamentoAleatorio(jogador: Personagem): void {
+    if (Math.random() >= 0.5) {
+      this.atacar(jogador);
+    }
+  }
+}
